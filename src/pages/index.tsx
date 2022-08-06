fb34b9c039a797c97978261fb7b806f1d674f448
marciocamello <marciocamello@oulook.com>
2022-08-06 13:42:27 +0100

    Add post page and create Prismic slice and types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
@@ -24,13 +26,66 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <div className={commonStyles.container}>
+      <header className={styles.header}>
+        <img src="/assets/logo.svg" alt="" />
+      </header>
+      <main className={styles.articles}>
+        <article className={styles.article}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.articleInfo}>
+            <div>
+              <FiCalendar size={18} />
+              <time dateTime="2020-01-01">15 Mar 2021</time>
+            </div>
+            <div>
+              <FiUser size={18} /> <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </article>
+        <article className={styles.article}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.articleInfo}>
+            <div>
+              <FiCalendar size={18} />
+              <time dateTime="2020-01-01">15 Mar 2021</time>
+            </div>
+            <div>
+              <FiUser size={18} /> <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </article>
+        <article className={styles.article}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.articleInfo}>
+            <div>
+              <FiCalendar size={18} />
+              <time dateTime="2020-01-01">15 Mar 2021</time>
+            </div>
+            <div>
+              <FiUser size={18} /> <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </article>
+      </main>
+      <div className={styles.loadingMoreArticles}>
+        <button type="button">Carregar mais artigos</button>
+      </div>
+      <div className={styles.previewButton}>
+        <button type="button">Sair do modo Preview</button>
+      </div>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+// const prismic = getPrismicClient({});
+// const postsResponse = await prismic.getByType(TODO);
 
-//   // TODO
+// TODO
 // };
